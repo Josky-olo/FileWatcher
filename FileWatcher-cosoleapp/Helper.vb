8b5cb28cb25abd5de07395ec32307c52d7fc85eb
Josky-olo <56986562+Josky-olo@users.noreply.github.com>
2021-06-28 08:40:41 +0700

    Add project files.

---
+++ FileWatcher-cosoleapp/Helper.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Configuration
+Imports System.Drawing
+Imports System.Net.Mail
+Imports System.Runtime.CompilerServices
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms
+
+Module Helper
+    <Extension()> Friend Function GetSetting(ByVal Key As String) As String
+        Dim Result As String = Nothing
+        Dim _appSettings = ConfigurationSettings.AppSettings
+        Try
+            If _appSettings.Count = 0 Then
+            Else
+                Result = _appSettings(Key)
+            End If
+        Catch Ex As ConfigurationErrorsException
+        End Try
+        Return Result
+    End Function
+    <Extension()> Friend Sub NotifierManager(title As String, msg As String)
+        PutNotifyIcon(title, msg)
+        SendMail(title, msg)
+    End Sub
+    <Extension()> Friend Sub PutNotifyIcon(title As String, msg As String)
+        Try
+            _Notify.Visible = True
+            _Notify.Icon = SystemIcons.Application
+            _Notify.BalloonTipIcon = ToolTipIcon.Info
+            _Notify.BalloonTipTitle = "Watcher alert " + title
+            _Notify.BalloonTipText = msg
+            _Notify.ShowBalloonTip(50000)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    <Extension()> Friend Sub SendMail(ByVal _subject As String, ByVal _msgBody As String)
+        Try
+            Dim SmtpServer As New SmtpClient()
+            Dim mail As New MailMessage()
+            SmtpServer.Credentials = New _
+        Net.NetworkCredential(GetSetting("Sender_Default"), GetSetting("Sender_Default_Password"))
+            SmtpServer.Port = 587
+            SmtpServer.Host = "smtp.gmail.com"
+            SmtpServer.EnableSsl = True
+            'SmtpServer.UseDefaultCredentials = False 
+            mail.From = New MailAddress(GetSetting("Sender_Default"))
+            mail.To.Add(GetSetting("Receiver_Default"))
+            mail.Subject = "Watcher alert " + _subject
+            mail.Body = _msgBody
+            SmtpServer.Send(mail)
+        Catch ex As Exception
+        End Try
+    End Sub
+    <Extension()> Friend Function EmailValid(ByVal _MailStr As String) As Boolean
+        Return Regex.IsMatch(_MailStr, "(?<=[_a-z0-9-]+(.[a-z0-9-]+)@[mM]axxis.co.th\z)")
+    End Function
+End Module
---
+++ FileWatcher-cosoleapp/program.vb
@@ -0,0 +1,88 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Windows.Forms
+
+Module program
+    Public ContextMenu1 As New ContextMenu
+    Public _Notify As New NotifyIcon
+    Private mre As New ManualResetEvent(False)
+    Private Declare Auto Function ShowWindow Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nCmdShow As Integer) As Boolean
+    Private Declare Auto Function GetConsoleWindow Lib "kernel32.dll" () As IntPtr
+    Sub Main()
+        Dim hWndConsole As IntPtr
+        hWndConsole = GetConsoleWindow()
+        ShowWindow(hWndConsole, Int16.Parse(GetSetting("SW_HIDE")))
+        Using watcher = New FileSystemWatcher(GetSetting("path_file"))
+            watcher.NotifyFilter = NotifyFilters.Attributes Or
+                                   NotifyFilters.CreationTime Or
+                                   NotifyFilters.DirectoryName Or
+                                   NotifyFilters.FileName Or
+                                   NotifyFilters.LastAccess Or
+                                   NotifyFilters.LastWrite Or
+                                   NotifyFilters.Security Or
+                                   NotifyFilters.Size
+
+            AddHandler watcher.Changed, AddressOf OnChanged
+            AddHandler watcher.Created, AddressOf OnCreated
+            AddHandler watcher.Deleted, AddressOf OnDeleted
+            AddHandler watcher.Renamed, AddressOf OnRenamed
+            AddHandler watcher.Error, AddressOf OnError
+
+            watcher.Filter = "*." + GetSetting("format_file")
+            watcher.IncludeSubdirectories = True
+            watcher.EnableRaisingEvents = True
+
+            Console.WriteLine("Watcher file (" + GetSetting("format_file") + ") at " + GetSetting("path_file") + " is started...")
+            Console.ReadLine()
+        End Using
+    End Sub
+
+
+    Private Sub OnChanged(sender As Object, e As FileSystemEventArgs)
+        If ((GetSetting("event_on_changed") = "no")) Then Exit Sub
+        If e.ChangeType <> WatcherChangeTypes.Changed Then
+            Return
+        End If
+        NotifierManager("Changed", $"File: {e.FullPath} have been changed")
+        Console.WriteLine($"Changed: {e.FullPath} {Now.ToString("dd-MM-yyyy hh:mm:ss")}")
+    End Sub
+
+    Private Sub OnCreated(sender As Object, e As FileSystemEventArgs)
+        If ((GetSetting("event_on_created") = "no")) Then Exit Sub
+        Dim value As String = $"Created: {e.FullPath}"
+        NotifierManager("Created", $"File: {e.FullPath} have been created")
+        Console.WriteLine(value + Now.ToString("dd-MM-yyyy hh:mm:ss"))
+    End Sub
+
+    Private Sub OnDeleted(sender As Object, e As FileSystemEventArgs)
+        If ((GetSetting("event_on_deleted") = "no")) Then Exit Sub
+        NotifierManager("Deleted", $"File: {e.FullPath} have been deleted")
+        Console.WriteLine($"Deleted: {e.FullPath} {Now.ToString("dd-MM-yyyy hh:mm:ss")}")
+    End Sub
+
+    Private Sub OnRenamed(sender As Object, e As RenamedEventArgs)
+        If ((GetSetting("event_on_renanmed") = "no")) Then Exit Sub
+        NotifierManager("Renamed", $"File: {e.OldFullPath} have been renamed")
+        Console.WriteLine($"Renamed: {Now.ToString("dd-MM-yyyy hh:mm:ss")}")
+        Console.WriteLine($"    Old: {e.OldFullPath}")
+        Console.WriteLine($"    New: {e.FullPath}")
+    End Sub
+
+    Private Sub OnError(sender As Object, e As ErrorEventArgs)
+        If ((GetSetting("event_on_errored") = "no")) Then Exit Sub
+        PrintException(e.GetException())
+    End Sub
+
+    Private Sub PrintException(ex As Exception)
+        If ex IsNot Nothing Then
+            NotifierManager("Errored", ex.Message)
+            Console.WriteLine({Now.ToString("dd-MM-yyyy hh:mm:ss")})
+            Console.WriteLine($"Message: {ex.Message}")
+            Console.WriteLine("Stacktrace:")
+            Console.WriteLine(ex.StackTrace)
+            Console.WriteLine()
+            PrintException(ex.InnerException)
+        End If
+    End Sub
+
+End Module
